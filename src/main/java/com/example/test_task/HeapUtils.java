ac7775b9322275eafdd40a625b6a85293821d7e0
Maksim Zadorozhnyi <m.zadorozhnyi@projectfirst.tv>
2025-11-07 00:05:35 +0300

    remove unused code

--- src/main/java/com/example/test_task/HeapUtils.java
+++ src/main/java/com/example/test_task/HeapUtils.java
@@ -2,32 +2,6 @@ package com.example.test_task;
 
 public class HeapUtils {
 
-    public static void siftDownMinHeap(int[] heapArr, int i, int length) {
-        while (2 * i + 1 < length) {
-            int childInd = 2 * i + 1;
-            if (2 * i + 2 < length && heapArr[2 * i + 2] < heapArr[2 * i + 1]) {
-                childInd = 2 * i + 2;
-            }
-            if (heapArr[i] <= heapArr[childInd]) {
-                break;
-            }
-            swap(heapArr, i, childInd);
-            i = childInd;
-        }
-    }
-
-    public static void siftUpMinHeap(int[] heapArr, int i) {
-        while (i - 1 >= 0) {
-            int parent = (i - 1) / 2;
-
-            if (heapArr[i] >= heapArr[parent]) {
-                break;
-            }
-            swap(heapArr, i, parent);
-            i = parent;
-        }
-    }
-
     public static void siftDownMaxHeap(int[] heapArr, int i, int length) {
         while (2 * i + 1 < length) {
             int childInd = 2 * i + 1;
@@ -53,7 +27,6 @@ public class HeapUtils {
             i = parent;
         }
     }
-
 
     private static void swap(int[] heapArr, int firstNode, int secondNode) {
         int tempVal = heapArr[firstNode];
